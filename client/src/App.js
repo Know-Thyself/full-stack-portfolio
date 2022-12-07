54fc13dfe41f04648e0889f46d595acb0c6087dc
Biruk Kebede <biruk.key@gmail.com>
2022-12-07 11:40:20 +0000

    restructured browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
+import { BrowserRouter } from 'react-router-dom';
 import useLocalStorage from 'use-local-storage';
 import Router from './Router';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	const [theme, setTheme] = useLocalStorage(
 		'theme',
 		defaultDark ? 'dark' : 'light'
 	);
 
-  return (
-		<div className='App' data-theme={theme}>
-			<Router theme={theme} setTheme={setTheme} />
-		</div>
+	return (
+		<BrowserRouter>
+			<div className='App' data-theme={theme}>
+				<Header theme={theme} setTheme={setTheme} />
+				<Router theme={theme} setTheme={setTheme} />
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- client/src/Router.js
+++ client/src/Router.js
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import ProjectDetails from './pages/ProjectDetails';
+
 const Router = () => {
 	const [projects, setProjects] = useState([]);
 	const [clickedProject, setClickedProject] = useState();
@@ -17,28 +17,30 @@ const Router = () => {
 			})
 			.catch((error) => console.error(error));
 	}, []);
-  return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route exact path='/' element={<Home />} />
-				<Route
-					exact
-					path='/projects'
-					element={
-						<Projects
-							projects={projects}
-							clickedProject={clickedProject}
-							setClickedProject={setClickedProject}
-						/>
-					}
-				/>
-				<Route exact path='/about' element={<About />} />
-				<Route exact path='/contact' element={<Contact />} />
-				<Route exact path='/details' element={<ProjectDetails clickedProject={clickedProject} />} />
-			</Routes>
-		</BrowserRouter>
+
+	return (
+		<Routes>
+			<Route exact path='/' element={<Home />} />
+			<Route
+				exact
+				path='/projects'
+				element={
+					<Projects
+						projects={projects}
+						clickedProject={clickedProject}
+						setClickedProject={setClickedProject}
+					/>
+				}
+			/>
+			<Route exact path='/about' element={<About />} />
+			<Route exact path='/contact' element={<Contact />} />
+			<Route
+				exact
+				path='/details'
+				element={<ProjectDetails clickedProject={clickedProject} />}
+			/>
+		</Routes>
 	);
-}
+};
 
 export default Router;
