887a9d07f6bda3becba3197601b9eebe3fd43e7f
Biruk Kebede <biruk.key@gmail.com>
2022-12-12 02:06:40 +0000

    about page minimal structure

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,45 @@
 const About = () => {
 	return (
-		<div>
-			<h1>About Page</h1>
-		</div>
+		<main className='about-page-main-container'>
+			<div className='about-page-wrapper'>
+				<div className='about-img-wrapper'>
+					<img
+						className='about-img'
+						src='/images/minimal-code-wallpaper.jpg'
+						alt=''
+					/>
+				</div>
+				<div>
+					<h1>About Page</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Cras
+						adipiscing enim eu turpis. Leo vel fringilla est ullamcorper eget
+						nulla facilisi etiam. Rhoncus dolor purus non enim praesent
+						elementum facilisis. Pellentesque adipiscing commodo elit at
+						imperdiet dui accumsan. Elementum nisi quis eleifend quam adipiscing
+						vitae proin sagittis. In est ante in nibh mauris cursus mattis
+						molestie a. Consequat interdum varius sit amet mattis vulputate
+						enim. Nunc non blandit massa enim nec dui. A iaculis at erat
+						pellentesque adipiscing commodo. Convallis aenean et tortor at
+						risus. Porta nibh venenatis cras sed. Commodo sed egestas egestas
+						fringilla. Sed viverra ipsum nunc aliquet bibendum enim facilisis
+						gravida neque. Tellus elementum sagittis vitae et leo duis. Quis
+						commodo odio aenean sed. Quis viverra nibh cras pulvinar mattis nunc
+						sed. Ut consequat semper viverra nam libero justo laoreet sit amet.
+						Vel pretium lectus quam id leo in vitae turpis massa. Eu facilisis
+						sed odio morbi quis commodo odio aenean sed.
+					</p>
+				</div>
+			</div>
+			<section className='skills-section'>
+				<div className='tech-stacks skill-set'>
+					<div className='skill-card'></div>
+					<div className='skill-card'></div>
+					<div className='skill-card'></div>
+				</div>
+			</section>
+		</main>
 	);
 };
 
